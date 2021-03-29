4ea74d924560924894137dd6925f08b31afb8415
misawa <h.misawa.sys20@morijyobi.ac.jp>
2021-03-29 12:08:41 +0900

    bmi測定アプリ

---
+++ bmi_main.py
@@ -0,0 +1,6 @@
+import calc_bmi
+
+h = int(input("身長:"))
+w = int(input("体重:"))
+print(f"bmi:{calc_bmi.get_area(h,w)}")
+print(f"bmi:{calc_bmi.get_area(h,w)}")
---
+++ calc_bmi.py
@@ -0,0 +1,3 @@
+def get_area(h,w):
+    bmi = w / h / h
+    return bmi
